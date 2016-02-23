24c1574e7b341345d9c850c257a67f14a73ae6e9
Aurélien Franky <au.re@live.com>
2016-02-23 11:31:58 +0000

    feat(placeholder): Placeholder function now returns the correct answer to life, the universe and eve

--- src/index.js
+++ src/index.js
@@ -1,66 +1,30 @@
 'use strict';
 
 /**
- *  Generate samples of synthetic data sets. 
- *  Ported from scikit-learn (https://github.com/scikit-learn/scikit-learn)
- *  @module SampleGenerator
+ *  Entry point to your node module
+ *  @module NodeScaffold
  * 
  */
 module.exports = {
-  
+
   /**
-   * @typedef Blobs
-   * @type Object
+   *  Placeholder function 
    * 
-   * @property {array} X - array of shape [nSamples, nFeatures]
-   *  The generated samples.
-   * 
-   * @property {array} y - array of shape [nSamples]
-   *  The integer labels for cluster membership of each sample.
-   *
-   */
-    
-  /**
-   *  Generate isotropic Gaussian blobs for clustering. 
-   * 
-   *  @param {number} nSamples - optional (default=100) 
-   *    The total number of points equally divided among clusters.
-   * 
-   *  @param {number} nFeatures - optional (default=2)
-   *    The number of features for each sample.
-   * 
-   *  @param {number | array} centers - number or array of shape [n_centers, n_features], optional (default=3)
-   *    The number of centers to generate, or the fixed center locations.
-   * 
-   *  @param {number} cluster_std - optional (default=1.0)
-   *    The standard deviation of the clusters.
-   * 
-   *  @param {numbers} center_box - (min, max), optional (default=(-10.0, 10.0))
-   *    The bounding box for each cluster center when centers are
-   *    generated at random.
-   * 
-   *  @param {boolean} shuffle - boolean, optional (default=True)
-   *    Shuffle the samples.
-   * 
-   *  @param {number} random_state - optional (default=None)
-   *    If int, random_state is the seed used by the random number generator
+   *  @param {string} question - optional (default=None) 
+   *    A meaningfull question about life and the universe
    *  
-   * 
-   *  @return {Blobs} {X,y} - An object containing two arrays:
-   *    the generated samples and their cluster membership.
+   *  @return {string} - returns the string "42!"
    *    
-   *  
    *  @example
-   *  >>> const makeBlobs = require('sample-generator').makeBlobs;
-   *  >>> let blobs = makeBlobs(n_samples=10, centers=3, n_features=2,
-   *  ...                   random_state=0);
-   *  >>> print(X.shape)
-   *  (10, 2)
-   *  >>> y
-   *  array([0, 0, 1, 0, 2, 2, 2, 1, 1, 0])
+   *  >>> const scaffold = require('node-scaffold');
+   *  >>> scaffold.myFunction("What is the Answer to the Ultimate Question of 
+   *  ... Life, the Universe, and Everything?");
+   * 
+   *  "42!"
    * 
    */
-  makeBlobs : function makeBlobs( ){
-      return "ok";
+  myFunction: function myFunction(question) {
+    let answer = 42 + "!";
+    return answer;
   }
 };
